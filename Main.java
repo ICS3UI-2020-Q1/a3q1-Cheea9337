6ebd462aafed1cf5f389a8dddb9642f57b45b482
Cheea9337 <cheea9337@wrdsb.ca>
2020-10-01 14:03:05 +0000

    Finished maximum number problem

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Find the biggest integer between three numbers
+ * @author Aidan Cheesmond
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
     
+    // ask the user for three integers
+    System.out.println("Please enter three integers on separate lines");
+    int firstInteger = input.nextInt();
+    int secondInteger = input.nextInt();
+    int thirdInteger = input.nextInt();
+
+    if(firstInteger > secondInteger && firstInteger > thirdInteger){
+      System.out.println("Maximum: " + firstInteger);
+    } else if(secondInteger > firstInteger && secondInteger > thirdInteger){
+      System.out.println("Maximum: " + secondInteger);
+    } else if(thirdInteger > firstInteger && thirdInteger > secondInteger){
+      System.out.println("Maximum: " + thirdInteger);
+    } else {
+      System.out.println("There is no maximum number");
+    }
+
   }
 }
